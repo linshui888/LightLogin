50b26a80ace9ea827cb65db2acb8bf0632a6cfdc
CMarcoo <cmarco.org@gmail.com>
2024-01-31 14:40:20 +0100

    First commit: Project Structure.

---
+++ src/main/java/top/cmarco/lightlogin/LightLoginPlugin.java
@@ -0,0 +1,25 @@
+package top.cmarco.lightlogin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LightLoginPlugin extends JavaPlugin {
+
+    /**
+     * Plugin startup logic.
+     * Called on STARTUP phase.
+     */
+    @Override
+    public void onEnable() {
+
+    }
+
+    /**
+     * Plugin loading logic.
+     * Called on LOAD phase.
+     */
+    @Override
+    public void onLoad() {
+
+    }
+
+}
