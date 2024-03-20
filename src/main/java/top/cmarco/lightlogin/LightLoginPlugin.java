52392d776db94cce9ece514e3683386d23b8b8e2
CMarcoo <cmarco.org@gmail.com>
2024-03-20 23:55:43 +0100

    Added Metrics.

--- src/main/java/top/cmarco/lightlogin/LightLoginPlugin.java
+++ src/main/java/top/cmarco/lightlogin/LightLoginPlugin.java
@@ -20,6 +20,7 @@ package top.cmarco.lightlogin;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.apache.logging.log4j.core.filter.AbstractFilter;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -43,6 +44,8 @@ import java.util.Objects;
 
 public final class LightLoginPlugin extends JavaPlugin {
 
+    private static final int METRICS_ID = 21378;
+
     private AbstractFilter safetyFilter = null;
     private LightConfiguration lightConfiguration = null;
     private PluginDatabase database = null;
@@ -59,6 +62,7 @@ public final class LightLoginPlugin extends JavaPlugin {
     private LibraryManager libraryManager = null;
     private CommandManager commandManager = null;
     private ListenerManager listenerManager = null;
+    private Metrics dataStealer = null;
 
     /* --------------------------------------------------------- */
 
@@ -84,6 +88,7 @@ public final class LightLoginPlugin extends JavaPlugin {
         this.setupCommands();
         this.setupKickManager();
         this.setAuthLogs();
+        this.startMetrics();
     }
 
     /**
@@ -98,9 +103,23 @@ public final class LightLoginPlugin extends JavaPlugin {
         if (this.authLogs != null) {
             this.authLogs.saveLogs();
         }
+
+        this.stopMetrics();
     }
 
     /* --------------------------------------------------------- */
+
+    private void startMetrics() {
+        this.dataStealer = new Metrics(this, METRICS_ID);
+    }
+
+    private void stopMetrics() {
+        if (this.dataStealer == null) {
+            return;
+        }
+
+        this.dataStealer.shutdown();
+    }
 
     private void loadLibraries() {
         this.libraryManager = new LibraryManager(this);
